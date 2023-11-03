37f5b659b229ff335b2dd718da72d396cceef2ba
omiras <oscar.miras@gmail.com>
2023-11-03 08:18:54 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let btnToggleDarkMode = document.querySelector("#btn-toggle-dark-mode");
+
+function toggleDarkMode() {
+  document.querySelector("html").classList.toggle("dark-mode");
+  btnToggleDarkMode.children[0].classList.toggle("bi-moon");
+  btnToggleDarkMode.children[0].classList.toggle("bi-moon-fill");
+}
+
+function init() {
+  btnToggleDarkMode.addEventListener("click", toggleDarkMode);
+
+}
+
+window.onload = init();
